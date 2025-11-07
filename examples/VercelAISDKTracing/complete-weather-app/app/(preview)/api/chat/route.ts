8f85799f4b407471271c8580e665d5dc7ebac713
Olmo Maldonado <olmo.maldonado@gmail.com>
2025-11-06 16:26:52 -0800

    update examples to avoid using deprecated ai-sdk interfaces (#80)

--- examples/VercelAISDKTracing/complete-weather-app/app/(preview)/api/chat/route.ts
+++ examples/VercelAISDKTracing/complete-weather-app/app/(preview)/api/chat/route.ts
@@ -1,8 +1,9 @@
 import { openai } from "@ai-sdk/openai";
-import { streamText, ToolInvocation } from "ai";
+import * as ai from "ai";
+import { ToolInvocation } from "ai";
 import { getWeather, getFahrenheit } from "@/components/tools";
 // Uncomment below to use Braintrust's tracing features
-// import { initLogger, wrapAISDKModel, traced, currentSpan } from "braintrust";
+// import { initLogger, wrapAISDK, traced, currentSpan } from "braintrust";
 
 // Initialize Braintrust as the logging backend. Uncomment below
 // const logger = initLogger({
@@ -10,11 +11,14 @@ import { getWeather, getFahrenheit } from "@/components/tools";
 //  projectName: process.env.BRAINTRUST_PROJECT_NAME,
 // });
 
-// Any time this model is called, the input and output will be logged to Braintrust. Uncomment below
-const model = //wrapAISDKModel(
-  openai("gpt-4o")
+// Any time streamText is called, the input and output will be logged to Braintrust. Uncomment below
+const {
+  streamText,
+} = //wrapAISDK(
+  ai;
 // Uncomment below
 // );
+const model = openai("gpt-4o");
 
 interface Message {
   role: "user" | "assistant";
@@ -26,36 +30,36 @@ export async function POST(request: Request) {
   // traced starts a trace span when the POST endpoint is used
   // Unlike wrapTraced, traced does not natively log inputs and outputs. Uncomment below
   // return traced(async (span) => {
-      const { messages }: { messages: Message[] } = await request.json();
+  const { messages }: { messages: Message[] } = await request.json();
 
-      const stream = await streamText({
-        // Our wrapped OpenAI model
-        model: model,
-        system: `\
+  const stream = await streamText({
+    // Our wrapped OpenAI model
+    model: model,
+    system: `\
         - you are an AI assistant who gives the weather. If the user gives you a location, give them the current weather in that location in Fahrenheit.
       `,
-        messages: messages,
-        // Important: maxSteps prevents infinite tool call loops but will stop your LLM's logic prematurely if set too low
-        maxSteps: 5,
-        // Register the exported tools to the LLM from @/components/tools
-        tools: {
-          getWeather: getWeather,
-          getFahrenheit: getFahrenheit,
-        },
-        // Enable experimental telemetry
-        experimental_telemetry: {
-          isEnabled: true,
-        },
-      // When streamText is finished, log the input and output of the stream for the "root" span. Uncomment below
-      // onFinish: (result) => {
-      //   currentSpan().log({
-      //     input: messages,
-      //     output: result.text,
-      //   });
-      // },
-        });
+    messages: messages,
+    // Important: maxSteps prevents infinite tool call loops but will stop your LLM's logic prematurely if set too low
+    maxSteps: 5,
+    // Register the exported tools to the LLM from @/components/tools
+    tools: {
+      getWeather: getWeather,
+      getFahrenheit: getFahrenheit,
+    },
+    // Enable experimental telemetry
+    experimental_telemetry: {
+      isEnabled: true,
+    },
+    // When streamText is finished, log the input and output of the stream for the "root" span. Uncomment below
+    // onFinish: (result) => {
+    //   currentSpan().log({
+    //     input: messages,
+    //     output: result.text,
+    //   });
+    // },
+  });
 
-      return stream.toDataStreamResponse();
-    }
-    // Show the this span as a function and name the span POST /api/chat. Uncomment below
-    // ,{ type: "function", name: "POST /api/chat" });}
+  return stream.toDataStreamResponse();
+}
+// Show the this span as a function and name the span POST /api/chat. Uncomment below
+// ,{ type: "function", name: "POST /api/chat" });}
